bd5013adbc7e36a72dcb7c4aaf4858664c8ae55f
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2022-02-19 12:22:10 +0530

    Updated uploading images function

--- index.js
+++ index.js
@@ -6,32 +6,43 @@ const fs = require("fs");
 const { promisify } = require("util");
 const unlinkAsync = promisify(fs.unlink);
 const app = express();
-var cors = require("cors");
 
-app.use(cors());
-
-app.listen(process.env.PORT || 3200, () => {
-  console.log("Server Connceted");
-});
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-const multer = require("multer");
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
 
-const upload = multer({ dest: "uploads/" });
-app.get("/", (req, res) => {
-  res.send("server is running");
+var cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Connected to port ${process.env.PORT || 8000}`);
 });
-app.post("/addImages", upload.array("photo"), async (req, res) => {
+
+app.get("/", (req, res) => {
+  res.send(`Server is up and running`);
+});
+
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+app.post("/uploadImages", upload.array("photos"), async (req, res) => {
   let images = {};
-  for (let i = 0; i < req.files.length; i++) {
-    const result = await cloudinary.uploader.upload(req.files[i].path);
-    images[`${i}`] = result.url;
-    await unlinkAsync(req.files[i].path);
-  }
-  res.status(200).send(images);
+  let noOfUploads = req.files.length;
+  req.files.forEach(async (file, i) => {
+    try {
+      const result = await cloudinary.uploader.upload(file.path);
+      images[`${i}`] = result.url;
+      await unlinkAsync(file.path);
+    } catch {
+      images[`${i}`] = "";
+    }
+    noOfUploads--;
+    if (noOfUploads === 0) {
+      res.status(200).send(images);
+    }
+  });
 });
